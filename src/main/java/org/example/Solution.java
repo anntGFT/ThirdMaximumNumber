8b9708523166b6647ecabb91eb55c8d7f1258c31
annt <annt@gft.com>
2022-12-05 17:04:00 +0000

    done with streams

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,2 +1,23 @@
-package org.example;public class Solution {
+package org.example;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution {
+    public int thirdMax(int[] nums) {
+
+       nums = Arrays.stream(nums)
+               .distinct()
+               .sorted()
+               .toArray();
+
+        if (nums.length == 1 || nums.length == 2){
+
+            return Arrays.stream(nums)
+                    .max()
+                    .getAsInt();
+        }
+
+        return nums[nums.length - 3];
+    }
 }
--- src/test/java/com/example/SolutionTest.java
+++ src/test/java/com/example/SolutionTest.java
@@ -1,2 +1,61 @@
-package com.example;public class SolutionTest {
+package com.example;
+
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+    
+    static Solution solution;
+
+    int result;
+    int[] nums;
+    
+    @BeforeAll
+    static void setup(){
+        
+        solution = new Solution();
+    }
+
+    @Test
+    public void onlyOneNumber(){
+
+        nums = new int[] {1};
+        result = 1;
+
+        assertEquals(result, solution.thirdMax(nums));
+
+    }
+
+    @Test
+    public void TwoNumbers(){
+
+        nums = new int[] {1,2};
+        result = 2;
+
+        assertEquals(result, solution.thirdMax(nums));
+
+    }
+
+    @Test
+    public void ThreeNumbers(){
+
+        nums = new int[] {1,3,2};
+        result = 1;
+
+        assertEquals(result, solution.thirdMax(nums));
+
+    }
+
+    @Test
+    public void FiveNumbers(){
+
+        nums = new int[] {1,33,44,73,2};
+        result = 33;
+
+        assertEquals(result, solution.thirdMax(nums));
+
+    }
 }
